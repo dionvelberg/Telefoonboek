2c5690fc9874f0390374262f2c6079a1e49b3771
dionvelberg <dion.thomas@hotmail.com>
2022-08-21 21:43:05 +0200

    Comments

--- Telefoonboek/Form1.cs
+++ Telefoonboek/Form1.cs
@@ -7,17 +7,17 @@ namespace Telefoonboek
             InitializeComponent();
         }
 
-        private void ShowNames_Click(object sender, EventArgs e)
+        private void ShowNames_Click(object sender, EventArgs e)// Show button click event
         {
-            string[] names = { "Frits", "Joost", "Karin" };
-            foreach (String name in names)
+            string[] names = { "Frits", "Joost", "Karin" };// Declare names & initialize names with default values
+            foreach (String name in names)// Foreach to add each name to the listbox
                 listBoxNames.Items.Add("Student: " + name);
         }
 
-        private void AddName_Click(object sender, EventArgs e)
+        private void AddName_Click(object sender, EventArgs e)// Add button click event
         {
-            string newName = textBoxAddName.Text;
-            listBoxNames.Items.Add("Student: " + newName);
+            string newName = textBoxAddName.Text;// Get textbox input
+            listBoxNames.Items.Add("Student: " + newName);// Add input to listbox
         }
 
     }
